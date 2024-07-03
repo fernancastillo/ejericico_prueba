8071b68d7465ad6b76668ddf1a4721a66e37fc74
FernandoCastillo <fern.castillop@duocuc.cl>
2024-07-03 09:27:11 -0400

    base del menú lista

---
+++ principal.py
@@ -0,0 +1,20 @@
+from funciones import *
+while True:
+    print("MENÚ GAXPLOSIVE:")
+    print("1. Registrar Pedido")
+    print("2. Listar todos los pedidos")
+    print("3. Buscar pedido por RUT")
+    print("4. Imprimir hoja de ruta")
+    print("5. Salir del programa")
+    opc=int(input("Ingrese un número del menú: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    elif opc==4:
+        pass
+    else:
+        print("Muchas gracias por usar la aplicación. Adios!")
+        break
