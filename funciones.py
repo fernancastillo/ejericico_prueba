0bfcefcac0258274eccf03aebc8ad16ab36b18e7
FernandoCastillo <fern.castillop@duocuc.cl>
2024-07-03 11:20:52 -0400

    ejercicio terminado con validaciones

--- funciones.py
+++ funciones.py
@@ -2,50 +2,91 @@ import os, msvcrt, csv, time
 pedidos=[]
 valor_5=12500
 valor_15=25500
-
 def menu_1():
-    print("REGISTRAR PEDIDO:")
-    rut=int(input("Ingrese el RUT: "))
-    nombre=input("Ingrese solamente el nombre del comprador: ")
-    direccion=input("Ingrese la dirección: ")
-    comuna=int(input("Ingrese el número de la comuna (1. Puente Alto - 2. San Bernardo - 3. San Joaquín): "))
+    print("\tREGISTRAR PEDIDO:\n")
+    while True:
+        try:
+            rut=int(input("Ingrese el RUT (sin guión ni digito verificador): "))
+            if len(str(rut))==8 or len(str(rut))==7:
+                break
+            else:
+                print("Error! Debe ingresar 7 u 8 dígitos!")
+        except:
+            print("Error! Debe ingresar un número entero!")
+    while True:
+        nombre=input("Ingrese solamente el nombre del comprador: ")
+        if len(nombre)<3:
+            print("Error! Debe ingresar mínimo 3 caracteres!")
+        elif not nombre.isalpha():
+            print("Error! Debe ingresar solo letras!")
+        else:
+            break
+    while True:
+        direccion=input("Ingrese la dirección: ")
+        if len(direccion)<5:
+            print("Error! Debe ingresar mínimo 5 caracteres!")
+        else:
+            break
+    while True:
+        try:
+            comuna=int(input("Ingrese el número de la comuna (1. Puente Alto - 2. San Bernardo - 3. San Joaquín): "))
+            if comuna in (1,2,3):
+                break
+            else:
+                print("Error! Debe ingresar un número del 1 al 3!")
+        except:
+            print("Error! Debe ingresar un número entero!")
     cont_5=0
     cont_15=0
+    print("A continuación se ingresará a un menú para agregar cilindros")
+    print("Presione una tecla para continuar")
+    msvcrt.getch()
     while True:
-        print("MENÚ CILINDROS")
+        os.system("cls")
+        print("\tMENÚ CILINDROS:\n")
         print("1. Agregar cilindro de 5 kg. ($ 12.500 c/u)")
         print("2. Eliminar un cilindro de 5 kg.")
         print("3. Agregar cilindro de 15 kg. ($ 25.500 c/u)")
         print("4. Eliminar cilindro de 15 kg.")
         print("5. Ver pedido actual")
         print("6. Terminar pedido")
-        opc_2=int(input("Ingrese un número del menú: "))
+        while True:
+            try:
+                opc_2=int(input("Ingrese un número del menú: "))
+                if opc_2 in (1,2,3,4,5,6):
+                    break
+                else:
+                    print("Error! Debe ingresar un número del menú!")
+            except:
+                print("Error! Debe ingresar un número entero!")
         os.system("cls")
         if opc_2==1:
             cont_5+=1
             print("Cilindro agregado con éxito!")
-            time.sleep (1.5)
+            time.sleep (1)
         elif opc_2==2:
             if cont_5==0:
                 print("Error! Primero debe agregar un cilindro!")
             else:
                 cont_5-=1
                 print("Cilindro eliminado con éxito!")
-            time.sleep (1.5)
+            time.sleep (1)
         elif opc_2==3:
             cont_15+=1
             print("Cilindro agregado con éxito!")
-            time.sleep (1.5)
+            time.sleep (1)
         elif opc_2==4:
             if cont_15==0:
                 print("Error! Primero debe agregar un cilíndro!")
             else:
                 cont_15-=1
                 print("Cilindro eliminado con éxito!")
-            time.sleep (1.5)
+            time.sleep (1)
         elif opc_2==5:
-            print("PEDIDO ACTUAL:")
-            print(f"Cilindros de 5 kg: {cont_5} - Cilindros de 15 kg {cont_15} - Total: $ {valor_5*cont_5+valor_15*cont_15}.")
+            print("\tPEDIDO ACTUAL:\n")
+            print(f"Cilindros de 5 kg: {cont_5} - Cilindros de 15 kg {cont_15} - Total: $ {valor_5*cont_5+valor_15*cont_15}.\n")
+            print("Presione una tecla para continuar")
+            msvcrt.getch()
         else:
             break
         if comuna==1:
@@ -54,26 +95,34 @@ def menu_1():
             comuna="San Bernardo"
         else:
             comuna="San Joaquín"
-        pedido={"rut":rut, 
-                "nombre":nombre,
-                "direccion":direccion, 
-                "comuna":comuna,
-                "cil_5kg":cont_5, 
-                "cil_15kg":cont_15, 
-                "total": valor_5*cont_5+valor_15*cont_15}
-        pedidos.append(pedido)
-        print("Pedido añadido con éxito!")
+    pedido={"rut":rut, 
+            "nombre":nombre,
+            "direccion":direccion, 
+            "comuna":comuna,
+            "cil_5kg":cont_5, 
+            "cil_15kg":cont_15, 
+            "total": valor_5*cont_5+valor_15*cont_15}
+    pedidos.append(pedido)
+    print("Pedido agregado con éxito!")
 def menu_2():
-    print("LISTAR TODOS LOS PEDIDOS: ")
+    print("\tLISTAR TODOS LOS PEDIDOS:\n")
     for p in pedidos:
-        print(f"RUT: {p['rut']} - Nombre: {p['nombre']} - Dirección: {p['direccion']} - Comuna: {p['comuna']} - Cil. 5 kg: {p['cil_5kg']} - Cil. 15 kg: {p['cil_15kg']} - Total: {p['total']}\n")
+        print(f"RUT: {p['rut']} - Nombre: {p['nombre']} - Dirección: {p['direccion']} - Comuna: {p['comuna']} - Cil. 5 kg: {p['cil_5kg']} - Cil. 15 kg: {p['cil_15kg']} - Total: {p['total']}")
 
 def menu_3():
     if len(pedidos)==0:
         print("Error! Aún no hay pedidos agregados!")
     else:
-        print("BUSCAR PEDIDO POR RUT:")
-        rut=int(input("Ingrese el RUT: "))
+        print("\tBUSCAR PEDIDO POR RUT:\n")
+        while True:
+            try:
+                rut=int(input("Ingrese el RUT (sin guión ni digito verificador): "))
+                if len(str(rut))==8 or len(str(rut))==7:
+                    break
+                else:
+                    print("Error! Debe ingresar 7 u 8 dígitos!")
+            except:
+                print("Error! Debe ingresar un número entero!")
         existe=False
         for p in pedidos:
             if p['rut']==rut:
@@ -85,22 +134,38 @@ def menu_4():
     if len(pedidos)==0:
         print("Error! Aún no hay pedidos agregados!")
     else:
-        print("IMPRIMIR HOJA DE RUTA")
-        ruta=int(input("Ingrese el número de la comuna (1. Puente Alto - 2. San Bernardo - 3. San Joaquín): "))
+        print("\tIMPRIMIR HOJA DE RUTA\n")
+        while True:
+            try:
+                ruta=int(input("Ingrese el número de la comuna (1. Puente Alto - 2. San Bernardo - 3. San Joaquín): "))
+                if ruta in (1,2,3):
+                    break
+                else:
+                    print("Error! Debe ingresar un número del 1 al 3!")
+            except:
+                print("Error! Debe ingresar un número entero!")
         if ruta==1:
             ruta="Puente Alto"
         elif ruta==2:
             ruta="San Bernardo"
         else:
             ruta="San Joaquín"
-        nombre=input("Ingrese nombre del archivo: ")
-        existe=False
-        with open (f"{nombre}.csv", "x", newline="") as archivo:
-            escritor=csv.DictWriter(archivo,['rut', 'nombre','direccion','comuna','cil_5kg','cil_15kg','total'])
-            escritor.writeheader()
-            for p in pedidos:
-                if p['comuna']==ruta:
-                    escritor.writerow(p)
-                    existe=True
-            if existe==False:
-                print("Error! Aún no existen rutas hacia la comuna ingresada!")
+        try:
+            nombre=input("Ingrese nombre del archivo: ")
+            if len(nombre)<3:
+                print("Error! Debe ingresar mínimo 3 caracteres!")
+            else:
+                existe=False
+                with open (f"{nombre}.csv", "x", newline="") as archivo:
+                    escritor=csv.DictWriter(archivo,['rut', 'nombre','direccion','comuna','cil_5kg','cil_15kg','total'])
+                    escritor.writeheader()
+                    for p in pedidos:
+                        if p['comuna']==ruta:
+                            escritor.writerow(p)
+                            existe=True
+                    if existe==False:
+                        print("Archivo creado, pero aún no existen rutas a esa comuna!")
+                    elif existe==True:
+                        print("Archivo creado con éxito!")
+        except:
+            print("Error! Ya existe un archivo ingresado con ese nombre!")
--- principal.py
+++ principal.py
@@ -5,13 +5,21 @@ while True:
     print("Presione una tecla para continuar")
     msvcrt.getch()
     os.system("cls")
-    print("MENÚ GAXPLOSIVE:")
-    print("1. Registrar Pedido")
+    print("\tMENÚ GAXPLOSIVE:\n")
+    print("1. Registrar pedido")
     print("2. Listar todos los pedidos")
     print("3. Buscar pedido por RUT")
     print("4. Imprimir hoja de ruta")
     print("5. Salir del programa")
-    opc=int(input("Ingrese un número del menú: "))
+    while True:
+        try:
+            opc=int(input("Ingrese un número del menú: "))
+            if opc in (1,2,3,4,5):
+                break
+            else:
+                print("Error! Debe ingresa un número del menú!")
+        except:
+            print("Error! Debe ingresar un número entero!")
     os.system("cls")
     if opc==1:
         menu_1()
